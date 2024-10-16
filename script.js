50ab3cf74ad5099503ca9e9c12163872c0204d69
mandal89 <39247808+mandal89@users.noreply.github.com>
2024-10-16 23:41:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-let t1 = gsap.timeline({ paused: true });
+let t1 = gsap.timeline({ paused: true }); 
+let flap = CSSRulePlugin.getRule(".envelope:before"); 
 
-t1.to('.letter', { duration: 0.5, ease: "power3.out", translateY: -100 })
-  .to('.shadow', { duration: 0.3, ease: "power2.out", scaleX: 2, opacity: 0.8 });
+t1.to(flap, { 
+    duration: 0.5, 
+    cssRule: {
+        rotateX: 180
+    }
+})
+.set(flap, {
+    cssRule: {
+        zIndex: 10
+    }
+})
+.to('.letter', {
+    translateY: -200,
+    duration: 0.9, 
+    ease: "back.inOut(1.5)"
+})
+.set('.letter', {
+    zIndex: 40
+})
+.to('.letter', {
+    duration: .7,  
+    ease: "back.out(.4)",
+    translateY: -5,
+    translateZ: 250
+});
 
-function openCard() {
-  t1.play();
-}
+let t2 = gsap.timeline({ paused: true }); 
+t2.to('.shadow', {
+    delay: 1.4,
+    width: 450,
+    boxShadow: "-75px 150px 10px 5px #eeeef3",
+    ease: "back.out(.2)",
+    duration: .7
+}); 
 
-let t2 = gsap.timeline({ paused: true });
-
-t2.to('.letter', { duration: 0.3, ease: "power2.in", translateY: 0 })
-  .to('.shadow', { duration: 0.2, ease: "power3.in", scaleX: 1, opacity: 0 });
-
-function closeCard() {
-  t2.play();
+function openCard(e) {
+    t1.play();
+    t2.play();
 }
 
 function closeCard(e) {
